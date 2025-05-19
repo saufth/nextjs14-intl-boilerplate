1b195ced1046765f72e23ecab651482329ec9c9b
saufth <saufth@gmail.com>
2025-05-19 14:03:27 -0600

    update site-footer author link

--- src/components/layouts/site-footer.tsx
+++ src/components/layouts/site-footer.tsx
@@ -14,9 +14,8 @@ export function SiteFooter () {
   return (
     <footer className='border-t bg-background'>
       <Shell>
-        <section className='flex items-center gap-x-4'>
-          <div className='flex-1 text-left text-sm leading-loose text-muted-foreground'>
-            {/* <Logotype size='sm' /> */}
+        <section className='flex items-center justify-between gap-x-4'>
+          <div className='text-sm text-muted-foreground'>
             {`${t('author')} `}
             <Link
               href={siteConfig.links.twitter}
