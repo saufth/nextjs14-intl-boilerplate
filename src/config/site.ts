fdc5069e25c8d7bfe523a7dd6f85fc9df97961af
saufth <saufth@gmail.com>
2025-05-19 14:07:06 -0600

    update site config links

--- src/config/site.ts
+++ src/config/site.ts
@@ -4,7 +4,7 @@ import { env } from '@/env.mjs'
 
 const links = {
   twitter: 'https://x.com/saufth',
-  github: 'https://github.com/saufth/xpassport',
+  github: 'https://github.com/saufth/nextjs-intl-boilerplate',
   githubAccount: 'https://github.com/saufth'
 }
 
